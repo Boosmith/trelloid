5aabeeba74c5e114196c8594c7fbbb8bdb9b079a
Andrew Smith <boosmith@gmail.com>
2019-08-07 16:59:51 +0100

    chore #2: Implement production build set up.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const webpackBundleAnalyzer = require("webpack-bundle-analyzer");
+const webPackBundleAnalyzer = require("webpack-bundle-analyzer");
 
 process.env.NODE_ENV = "production";
 
@@ -10,29 +10,24 @@ module.exports = {
 	mode: "production",
 	target: "web",
 	devtool: "source-map",
-	entry: "./src/index",
+	entry: ["./src/index"],
 	output: {
 		path: path.resolve(__dirname, "build"),
 		publicPath: "/",
 		filename: "bundle.js"
 	},
 	plugins: [
-		// Display bundle stats
-		new webpackBundleAnalyzer.BundleAnalyzerPlugin({ analyzerMode: "static" }),
-
+		new webPackBundleAnalyzer.BundleAnalyzerPlugin({ analyzerMode: "static" }),
 		new MiniCssExtractPlugin({
 			filename: "[name].[contenthash].css"
 		}),
-
 		new webpack.DefinePlugin({
-			// This global makes sure React is built in prod mode.
 			"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
 			"process.env.API_URL": JSON.stringify("http://localhost:3001")
 		}),
 		new HtmlWebpackPlugin({
 			template: "src/index.html",
 			minify: {
-				// see https://github.com/kangax/html-minifier#options-quick-reference
 				removeComments: true,
 				collapseWhitespace: true,
 				removeRedundantAttributes: true,
