6f3e143d3d1a19cf169577f954006f3da287d3bc
Andrew Smith <boosmith@gmail.com>
2019-08-07 22:36:37 +0100

    feature #4: Integrate with node api

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -24,7 +24,7 @@ module.exports = {
 	},
 	plugins: [
 		new webpack.DefinePlugin({
-			"process.env.API_URL": JSON.stringify("http://localhost:3001")
+			"process.env.API_URL": JSON.stringify("http://localhost:3010")
 		}),
 		new HtmlWebpackPlugin({
 			template: "src/index.html"
