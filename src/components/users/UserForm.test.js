fc18590fc7410cfea56d289aaac820a3946a43ca
Andrew Smith <boosmith@gmail.com>
2019-08-07 13:40:44 +0100

    test #2: Implement enzyme test suite.

--- src/components/users/UserForm.test.js
+++ src/components/users/UserForm.test.js
@@ -5,9 +5,22 @@ import { users } from "../../../tools/mockData";
 import { isTSAnyKeyword, exportAllDeclaration } from "@babel/types";
 import { JestEnvironment } from "@jest/environment";
 
-it("sets submit button to 'Saving...' when saving is true", () => {
+it("sets submit button label to 'Saving...' when saving is true", () => {
 	const tree = renderer.create(
 		<UserForm user={users[0]} onSave={jest.fn()} onChange={jest.fn()} saving />
+	);
+
+	expect(tree).toMatchSnapshot();
+});
+
+it("sets submit button label to 'Save' when saving is false", () => {
+	const tree = renderer.create(
+		<UserForm
+			user={users[0]}
+			onSave={jest.fn()}
+			onChange={jest.fn()}
+			saving={false}
+		/>
 	);
 
 	expect(tree).toMatchSnapshot();
