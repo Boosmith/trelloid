9142d0df018efdf469497a2fb46568ea5b92c1e4
Andrew Smith <boosmith@gmail.com>
2019-08-28 15:13:44 +0100

    chore #12: Update mockUsers to include userName and password

--- src/tools/generateCards.js
+++ src/tools/generateCards.js
@@ -1,5 +1,6 @@
 const randomWords = require("random-words");
 const bcrypt = require("bcrypt");
+const { users } = require("./mockData");
 
 const words = randomWords({ exactly: 10, wordsPerString: 2 });
 
@@ -30,4 +31,19 @@ const generateCards = function() {
 	console.log(cards);
 };
 
-module.exports = generateCards();
+const updateMockUsers = function() {
+	const updatedUsers = users.map(user => {
+		let userName = randomWords({ exactly: 2, join: " " });
+		let password = bcrypt.hashSync(userName, 5);
+		let newFields = {
+			userName: userName,
+			password: password
+		};
+		return Object.assign(user, newFields);
+	});
+	console.log(updatedUsers);
+};
+
+module.exports =
+	// generateCards,
+	updateMockUsers();
--- src/tools/mockData.js
+++ src/tools/mockData.js
@@ -168,7 +168,9 @@ const users = [
 		address: "32 Walpole Road",
 		city: "London",
 		postcode: "N17",
-		__v: 0
+		__v: 0,
+		userName: "claws circle",
+		password: "$2b$05$2.3R3gIiLY0.k3W2NyFU1eS0By2gEatUxvFNZ/hm6w.uLP4xynrZK"
 	},
 	{
 		_id: "5d43fed51d0825849c0f0b67",
@@ -177,7 +179,9 @@ const users = [
 		address: "32 Walpole Road",
 		city: "London",
 		postcode: "N17",
-		__v: 0
+		__v: 0,
+		userName: "court nation",
+		password: "$2b$05$N8m6IdK07d.ebxqkS2pvDuBAh8oVkeVblDr/D36u5GdK3eKa9OrWy"
 	},
 	{
 		_id: "5d42104a7979bd071c129604",
@@ -186,7 +190,9 @@ const users = [
 		address: "1300 Eknaw Path",
 		city: "Evegirhe",
 		postcode: "P8K 4T1",
-		__v: 0
+		__v: 0,
+		userName: "brass indeed",
+		password: "$2b$05$CB4O9hzF7Wx.C..hActQoOzjIiFVcKK79h6zJutK1seHj13b9i/gq"
 	},
 	{
 		_id: "5d42104a7979bd071c129605",
@@ -195,7 +201,9 @@ const users = [
 		address: "1050 Dedu Point",
 		city: "Irhekde",
 		postcode: "G1K 2N9",
-		__v: 0
+		__v: 0,
+		userName: "interior one",
+		password: "$2b$05$u3S3FSfVetkUrYAbQgUTROLbk7l4Ez3Z6PcQlp1XRiPoy83ult.BW"
 	},
 	{
 		_id: "5d42104a7979bd071c129606",
@@ -204,7 +212,9 @@ const users = [
 		address: "560 Fibab Glen",
 		city: "Hudmakap",
 		postcode: "T9U 0N6",
-		__v: 0
+		__v: 0,
+		userName: "season carried",
+		password: "$2b$05$d1NOjWSsLqvjssyI1LKaK.QcnofuAwjgIozR86Pn/p2ixA0CjVX06"
 	},
 	{
 		_id: "5d42104a7979bd071c129607",
@@ -213,7 +223,9 @@ const users = [
 		address: "1630 Zuas Place",
 		city: "Caturaagu",
 		postcode: "G9E 7N0",
-		__v: 0
+		__v: 0,
+		userName: "stay underline",
+		password: "$2b$05$MqkYhqxPam.a2LuUB7J9l.nDih2DRRIYxGoxh4SLzKxvA7W7Ltmjm"
 	},
 	{
 		_id: "5d42104a7979bd071c129608",
@@ -222,7 +234,9 @@ const users = [
 		address: "1689 Hopge Square",
 		city: "Vinpati",
 		postcode: "C2O 6D1",
-		__v: 0
+		__v: 0,
+		userName: "end owner",
+		password: "$2b$05$1ErQO/LyTVZzPqXy5fv2AOEQUBcw0cypVQffxXX7J/1qJphFckDbO"
 	},
 	{
 		_id: "5d42104a7979bd071c129609",
@@ -231,7 +245,9 @@ const users = [
 		address: "1912 Dusved Ridge",
 		city: "Atehasma",
 		postcode: "N2D 7C3",
-		__v: 0
+		__v: 0,
+		userName: "column location",
+		password: "$2b$05$BHvS9h08epkH8LL7CJ31HuCz09fYJ3DIv81w0BWZQu.mtoB0v7BV2"
 	},
 	{
 		_id: "5d42104a7979bd071c12960a",
@@ -240,7 +256,9 @@ const users = [
 		address: "611 Gumaf Way",
 		city: "Kalover",
 		postcode: "E4Y 2E7",
-		__v: 0
+		__v: 0,
+		userName: "action please",
+		password: "$2b$05$8TrKFk/gSO241uG/4s6k.uJWi/0tc0KZPCoqbVryAfVu/7YfgDqvy"
 	},
 	{
 		_id: "5d42104a7979bd071c12960b",
@@ -249,16 +267,19 @@ const users = [
 		address: "1961 Goku Heights",
 		city: "Kivzupzi",
 		postcode: "J5C 8H1",
-		__v: 0
+		__v: 0,
+		userName: "solution primitive",
+		password: "$2b$05$ZQXo4wwzA.wzrGKY7/BM8uthaaoYXvkFx8HbbUDlPTwXMAGZOKsu."
 	}
 ];
-
 const newUser = {
 	firstName: "",
 	lastName: "",
 	address: "",
 	city: "",
-	postcode: ""
+	postcode: "",
+	userName: "",
+	password: ""
 };
 
 module.exports = {
