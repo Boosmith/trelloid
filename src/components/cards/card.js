9e91bebb5a387d40169d69496ce3f827e5d15294
Andrew Smith <boosmith@gmail.com>
2019-08-31 18:48:16 +0100

    feature #11: Created card presentation component

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -1,2 +1,30 @@
 import React from "react";
-import Card from "@material-ui/core/Card";
+import UiCard from "@material-ui/core/Card";
+import { CardContent, CardHeader, Typography } from "@material-ui/core";
+
+const Card = ({
+	_id,
+	number,
+	title,
+	description,
+	status,
+	board,
+	comments,
+	activity,
+	attachments,
+	owner,
+	members,
+	createdDate,
+	modifiedDate
+}) => (
+	<UiCard>
+		<CardHeader title={title} />>
+		<CardContent>
+			<Typography variant="body1" gutterBottom>
+				{description}
+			</Typography>
+		</CardContent>
+	</UiCard>
+);
+
+export default Card;
