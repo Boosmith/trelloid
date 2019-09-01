149da853ad706594bfad9b0b5d95ddaba7961bb8
Andrew Smith <boosmith@gmail.com>
2019-09-01 18:50:50 +0100

    feature #11: Depopulate Cards

--- src/api/cardApi.js
+++ src/api/cardApi.js
@@ -2,6 +2,16 @@ import { handleResponse, handleError } from "./apiUtils";
 import { bearerHeaders } from "../tools/auth";
 const baseUrl = process.env.REACT_APP_API_URL + "/api/cards/";
 
+export function depopulateCard(card) {
+	card.owner = card.owner._id;
+	let members = [];
+	card.members.forEach(member => {
+		members.push(member._id);
+	});
+	card.members = members;
+	return card;
+}
+
 export function getCards() {
 	return fetch(baseUrl, bearerHeaders())
 		.then(handleResponse)
@@ -18,7 +28,7 @@ export function saveCard(Card) {
 	return fetch(baseUrl + (Card._id || ""), {
 		method: Card._id ? "PUT" : "POST",
 		headers: { "content-type": "application/json" },
-		body: JSON.stringify(Card)
+		body: JSON.stringify(depopulateCard(Card))
 	})
 		.then(handleResponse)
 		.catch(handleError);
